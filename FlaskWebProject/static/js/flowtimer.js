74d1a086ac5fe79187f2c39a2b89c31898513fd5
Chip Huyen <huyenn@stanford.edu>
2016-02-13 14:12:50 -0800

    Timing

--- FlaskWebProject/static/js/flowtimer.js
+++ FlaskWebProject/static/js/flowtimer.js
@@ -5,19 +5,28 @@
    analytics: "UA-73749016-1"
  };
 
+
  // bind listeners to all players on the page
  flowplayer(function(api, root) {
+
     var totalTime = 0;
     var endTime = 0;
     var startingTime;
        // when a new video is about to be loaded
+    
     api.on("load", function() {
-       console.info("load", api.engine.engineName);
+      console.info("Your video is loading ...", api.engine.engineName);
        // when a video is loaded and ready to play
     }).on("ready", function() {
-       console.info("ready", api.video.duration)
-       startingTime = new Date().getTime() / 1000;
-       console.info("starting", startingTime)
+      // var total = api.video.duration;
+      document.getElementById("time-spent").innerHTML = "To watch the whole video you'd need " + api.video.duration * 0.1 + " bitcoins";
+      startingTime = getTime("starting", totalTime);
+      // console.info("ready", api.video.duration)
+      // startingTime = new Date().getTime() / 1000;
+      // console.info("starting", startingTime)
+      // window.setInterval(function(){
+      //   console.log("Another second passed");
+      // }, 5000);
     }).on("pause", function() {
        var endTime = new Date().getTime() / 1000;
        console.info("end", endTime)
@@ -36,3 +45,14 @@
        console.info("you suck");
     });
  });
+
+function getTime(displayStr, totalTime) {
+  var time = new Date().getTime() / 1000;
+  console.info(displayStr, time);
+  window.setInterval(function(){
+    console.log("Another 5 seconds passed");
+    totalTime += 1;
+    document.getElementById("current-time-spent").innerHTML = "You have spent " + totalTime * 0.1 + " bitcoins on this video";
+  }, 1000);
+  return time;
+}
