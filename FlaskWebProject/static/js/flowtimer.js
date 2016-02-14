4f95efcb23984eee9aca212d5db6859fe2f5092b
Chip Huyen <huyenn@stanford.edu>
2016-02-13 23:01:24 -0800

    views working

--- FlaskWebProject/static/js/flowtimer.js
+++ FlaskWebProject/static/js/flowtimer.js
@@ -32,14 +32,15 @@ flowplayer(function(api, root) {
   thisView.transaction(function(current) {
     return current+1;
   });
+  // Display views
+
   var ref = new Firebase(url);
-  // Attach an asynchronous callback to read the data at our posts reference
   ref.on("value", function(snapshot) {
     document.getElementById("views-info").innerHTML = "This video has been viewed " + snapshot.val() + " times";
   }, function (errorObject) {
     console.log("The read failed: " + errorObject.code);
   });
-  // document.getElementById("views-info").innerHTML = "This video has been viewed " + dbRef[key]["views"] + " times";
+
   var endTime = 0;
   var startingTime;
   api.on("load", function() {
